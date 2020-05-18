4baeccc18d0c8c25e056f7711284b3e6bfd2457d
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-05-18 14:51:44 +0000

    Automatically backed up by Learn

---
+++ lib/user.rb
@@ -0,0 +1,24 @@
+class User 
+  
+  def initialize
+    @first_name
+    @last_name 
+  end 
+  
+  def first_name 
+    @first_name 
+  end 
+  
+  def first_name=(new_name) 
+    @first_name = new_name 
+  end 
+  
+    def last_name 
+    @last_name 
+  end 
+  
+  def last_name=(new_name) 
+    @last_name = new_name 
+  end 
+  
+end
